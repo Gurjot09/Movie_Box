64fbf9b64717bcfa8f12c1148217182665402db5
Gurjot09 <gurjot@maropost.com>
2017-12-28 13:47:38 +0000

    chenge in migration

--- db/migrate/20171218082613_change_column_in_movies.rb
+++ db/migrate/20171218082613_change_column_in_movies.rb
@@ -1,5 +1,5 @@
-class ChangeColumnInMovies < ActiveRecord::Migration[5.1]
-  def change
-   change_column :movies, :rating, :integer
-  end
-end
+# class ChangeColumnInMovies < ActiveRecord::Migration[5.1]
+#   def change
+#    change_column :movies, :rating, :integer
+#   end
+# end
