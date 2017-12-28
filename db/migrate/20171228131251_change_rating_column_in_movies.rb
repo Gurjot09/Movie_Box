0b0a22445f21c3ee5896c61d9bbba2383736d7cb
Gurjot09 <gurjot@maropost.com>
2017-12-28 14:34:02 +0000

    last commit

--- db/migrate/20171228131251_change_rating_column_in_movies.rb
+++ db/migrate/20171228131251_change_rating_column_in_movies.rb
@@ -1,7 +1,7 @@
 class ChangeRatingColumnInMovies < ActiveRecord::Migration[5.1]
   def change
   
-   change_column :movies, :rating, :float
+  # change_column :movies, :rating, :float
   end
   
 end
